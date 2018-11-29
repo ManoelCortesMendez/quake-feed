8886c696f37bf98814b863abd74174c9c6803df8
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-11-29 13:32:09 +0100

    Implement minimum magnitude setting

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +12,11 @@ import android.os.Bundle;
  */
 public class SettingsActivity extends AppCompatActivity {
 
+    /**
+     * Set activity content when activity is created.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +26,45 @@ public class SettingsActivity extends AppCompatActivity {
     /**
      * Settings preference fragment for encapsulating various settings.
      */
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
 
+            // Bind preference summary to preference values
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+        }
+
+        /**
+         * Method called when user changes a preference to update the UI.
+         *
+         * @param preference Preference changed.
+         * @param newValue New preference value.
+         * @return whether to update or not the state of the preference with the new value.
+         */
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            // The code in this method takes care of updating the displayed preference summary after it has been changed
+            String stringValue = newValue.toString();
+            preference.setSummary(stringValue);
+            return true; // To update the state of the preference with the new value
+        }
+
+        /**
+         * Helper method to bind preference summary to preference value via listener.
+         *
+         * @param preference Selected preference.
+         */
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            // Set the current EarthquakePreferenceFragment instance to listen for changes
+            preference.setOnPreferenceChangeListener(this);
+            // Get current preference value
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            // Display it in the preference summary -- that is, in the UI
+            onPreferenceChange(preference, preferenceString);
+        }
     }
 }
